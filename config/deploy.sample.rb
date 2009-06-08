75ce64be838e7cb64391f2acb8267b2304921ddd
Michael Kessler <michi@netzpiraten.ch>
2009-06-08 23:18:15 +0200

    Checkin deploy.sample.rb

--- config/deploy.sample.rb
+++ config/deploy.sample.rb
@@ -1,7 +1,7 @@
 # allocate a pty by default as some systems have problems without
 default_run_options[:pty] = true
 
-set :application, "extranett"
+set :application, "radiant-multisite"
 
 after "deploy", "deploy:cleanup"
 
@@ -10,7 +10,7 @@ after "deploy", "deploy:cleanup"
 # ================================================================
 
 set :scm, "git"
-set :repository,  "ssh://www.netzpiraten.ch/var/git/radiant-extranett.git"
+set :repository,  "git://github.com/netzpirat/radiant-multisite.git"
 
 set :branch, "master"
 set :git_enable_submodules, 1    #comment for cap deploy:setup
@@ -20,15 +20,15 @@ set :git_enable_submodules, 1    #comment for cap deploy:setup
 # ================================================================
 
 set :rails_env, "production"
-set :mod_rails_restart_file, "/var/www/www.extranett.ch/current/tmp/restart.txt"
+set :mod_rails_restart_file, "/var/www/www.radiant-multisite.org/current/tmp/restart.txt"
 
 # ================================================================
 # SERVERS
 # ================================================================
 
-set :deploy_to, "/var/www/www.extranett.ch"
+set :deploy_to, "/var/www/www.radiant-multisite.org"
 set :use_sudo, false
 
-role :web, "www.extranett.ch"
-role :app, "www.extranett.ch"
-role :db,  "www.extranett.ch", :primary => true
+role :web, "www.radiant-multisite.org"
+role :app, "www.radiant-multisite.org"
+role :db,  "www.radiant-multisite.org", :primary => true
