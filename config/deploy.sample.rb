b8ea60ca114e8a433be13886237544d37a53a9e8
Michael Kessler <michi@netzpiraten.ch>
2009-09-16 23:57:48 +0200

    Updated capistrano samples

--- config/deploy.sample.rb
+++ config/deploy.sample.rb
@@ -23,21 +23,3 @@ set :repository,  "git://github.com/netzpirat/radiant-multisite.git"
 set :branch, "master"
 set :git_enable_submodules, 1
 set :deploy_via, :remote_cache
-
-# ================================================================
-# RAILS
-# ================================================================
-
-set :rails_env, "production"
-set :mod_rails_restart_file, "/var/www/radiant-multisite/current/tmp/restart.txt"
-
-# ================================================================
-# SERVERS
-# ================================================================
-
-set :deploy_to, "/var/www/radiant-multisite"
-set :use_sudo, false
-
-role :web, "www.radiant-multisite.org"
-role :app, "www.radiant-multisite.org"
-role :db,  "www.radiant-multisite.org", :primary => true
--- config/deploy/production.sample.rb
+++ config/deploy/production.sample.rb
@@ -1,10 +1,17 @@
 # ================================================================
-# PRODUCTION SERVER
+# RAILS
 # ================================================================
 
-set :deploy_to, "/var/www/www.youserver.ch"
+set :rails_env, "production"
+set :mod_rails_restart_file, "/var/www/radiant-multisite/current/tmp/restart.txt"
+
+# ================================================================
+# SERVERS
+# ================================================================
+
+set :deploy_to, "/var/www/radiant-multisite"
 set :use_sudo, false
 
-role :web, "www.youserver.ch"
-role :app, "www.youserver.ch"
-role :db,  "www.youserver.ch", :primary => true
+role :web, "www.radiant-multisite.org"
+role :app, "www.radiant-multisite.org"
+role :db,  "www.radiant-multisite.org", :primary => true
