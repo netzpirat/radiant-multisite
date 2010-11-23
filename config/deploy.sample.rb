b6181ea42652306de28688517cc3a1378f7676c5
Michael Kessler <michi@netzpiraten.ch>
2010-11-23 12:26:37 +0100

    Add RVM to Capistrano

--- config/deploy.sample.rb
+++ config/deploy.sample.rb
@@ -1,16 +1,21 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
 # ================================================================
 # COMMON DEPLOYMENT OPTIONS
 # ================================================================
 
 set :stages, %w(production)
+
 require 'capistrano/ext/multistage'
 require 'capistrano/deepmodules'
 require 'bundler/capistrano'
+require 'rvm/capistrano'
 
 # allocate a pty by default as some systems have problems without
 default_run_options[:pty] = true
 
 set :application, "radiant-multisite"
+set :rvm_ruby_string, 'ruby-1.8.7@radiant-multisite'
 
 after "deploy", "deploy:cleanup"
 
